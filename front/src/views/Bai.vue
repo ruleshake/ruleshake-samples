68d7a094a103fbfac5c0c7202738c0725b2e46bb
Mohammed BOUKADA <m.boukada@gmail.com>
2024-05-23 19:46:09 +0200

    change bai

--- front/src/views/Bai.vue
+++ front/src/views/Bai.vue
@@ -2,8 +2,15 @@
   <div class="container flex lg:flex-row flex-col py-6 text-white align-top">
     <div class="lg:w-1/2 w-full px-3 mx-auto">
       <Form type="column" collection-code="BAI"
-            :ignored-variables="['FORMULES', 'GARANTIES', 'RESULT']"
+            :ignored-variables="['FORMULES', 'GARANTIES', 'ASSIETTES']"
             :initial-variables="inputs" @values="parseResult" @error="onError"/>
+      <div v-if="combinaisons" class="mt-8 flex justify-end">
+        <a href="#" @click="downloadJsonFile()"
+           class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center"
+        >
+          Télécharger
+        </a>
+      </div>
     </div>
     <div class="lg:w-1/2 w-full flex flex-col items-center px-3 mt-6">
       <div v-for="combinaison in combinaisons" class="p-2 bg-gray-200 text-gray-900 w-full">
@@ -16,7 +23,7 @@
             :aria-expanded="combinaison.expanded"
             aria-controls="faqs-text-01"
           >
-            <span>{{ combinaison.formule }} - {{ combinaison.total }} %</span>
+            <span>{{ combinaison.formule }}</span>
             <svg class="fill-indigo-500 shrink-0 ml-8" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
               <rect y="7" width="16" height="2" rx="1" class="transform origin-center transition duration-200 ease-out" :class="{'!rotate-180': combinaison.expanded}" />
               <rect y="7" width="16" height="2" rx="1" class="transform origin-center rotate-90 transition duration-200 ease-out" :class="{'!rotate-180': combinaison.expanded}" />
@@ -32,17 +39,21 @@
                 <thead>
                 <tr>
                   <th>Garantie</th>
-                  <th>Taux</th>
+                  <th>Taux couv</th>
+                  <th>Taux coti</th>
+                  <th>TBI</th>
+                  <th>NBI</th>
+                  <th>RI</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="garantie in combinaison.garanties">
-                  <td class="px-6">{{ garantie.label }}</td>
-                  <td class="px-6">{{ garantie.rate }}</td>
-                </tr>
-                <tr>
-                  <td class="px-6"></td>
-                  <td class="px-6">{{ combinaison.total }}</td>
+                  <td class="px-6">{{ garantie.code }}</td>
+                  <td class="px-6">{{ garantie.couv }}</td>
+                  <td class="px-6">{{ garantie.coti }}</td>
+                  <td class="px-6">{{ garantie.tbi }}</td>
+                  <td class="px-6">{{ garantie.nbi }}</td>
+                  <td class="px-6">{{ garantie.ri }}</td>
                 </tr>
                 </tbody>
               </table>
@@ -59,55 +70,67 @@ import {ref} from "vue";
 import {useNotification} from "@kyvg/vue3-notification";
 
 const inputs = [];
-['F90', 'F95'].forEach((formule, index) => inputs.push({
-  type: 'string',
-  reference: `FORMULES[${index}]`,
-  value: formule
-}));
-[["INC", "INV"], ["INC", "INV", "DECES"], ["INC", "INV", "DECES", "PATCA", "MATAD"], ["INC", "INV", "PATCA", "MATAD"]].forEach((combinaison, indexC) => {
-  combinaison.forEach((garantie, indexG) => {
-    inputs.push({
-      type: 'string',
-      reference: `COMBINAISONS[${indexC}]/GARANTIES[${indexG}]`,
-      value: garantie
-    })
-  })
-})
-
 const combinaisons = ref(null)
 const parseResult = (values) => {
   combinaisons.value = values
-    .filter(variable => variable.definitionReference === 'RESULT')
+    .filter(variable => variable.definitionReference === 'MODELES')
     .map(item => {
-      const {FORMULE, COMBINAISON} = item.loopStep;
-      const formule = values.find(variable => variable.runtimeReference === FORMULE).value
-      const codes = values.find(variable => variable.runtimeReference === COMBINAISON).subVariables
-        .map(g => g.value)
-        .join(" / ")
-      const total = item.subVariables.find(variable => variable.definitionReference === "RESULT/TOTAL").value
-      const garanties = item.subVariables.filter(variable => variable.definitionReference === "RESULT/GARANTIES")
+      const libelle = item.subVariables.find(variable => variable.definitionReference === "MODELES/LIBELLE").value
+      const total = item.subVariables.find(variable => variable.definitionReference === "MODELES/TAUX").value
+      const garanties = item.subVariables.filter(variable => variable.definitionReference === "MODELES/GARANTIES")
         .map(garantie => {
-          const libelle = garantie.subVariables
-            .find(variable => variable.definitionReference === "RESULT/GARANTIES/INFOS")
-            .subVariables
-            .find(variable => variable.definitionReference === "RESULT/GARANTIES/INFOS/LIBELLE")
+          const code = garantie.subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/CODE")
             .value
           const taux = garantie.subVariables
-            .find(variable => variable.definitionReference === "RESULT/GARANTIES/TAUX")
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/TAUX_COTI")
+            .value
+          const tauxCouv = garantie.subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/TAUX_COUV")
+            .value
+          const tbi = garantie.subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE")
+            .subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE/TBI")
+            .value
+          const nbi = garantie.subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE")
+            .subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE/NBI")
+            .value
+          const ri = garantie.subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE")
+            .subVariables
+            .find(variable => variable.definitionReference === "MODELES/GARANTIES/COUVERTURE/RI")
             .value
           return {
-            label: libelle,
-            rate: taux
+            code: code,
+            coti: taux,
+            couv: tauxCouv,
+            tbi: tbi,
+            nbi: nbi,
+            ri: ri
           }
         })
       return {
-        formule: formule + " - " + codes,
+        formule: libelle,
         total: total,
         garanties: garanties,
         expanded: false
       }
     });
-  console.log(combinaisons.value)
+}
+
+function downloadJsonFile() {
+  // Creating a blob object from non-blob data using the Blob constructor
+  const blob = new Blob([JSON.stringify(combinaisons.value)], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  // Create a new anchor element
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'bai.json';
+  a.click();
+  a.remove();
 }
 
 const {notify} = useNotification()
